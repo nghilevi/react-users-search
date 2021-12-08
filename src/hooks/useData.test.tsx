56dbc44b86f1f787958403755d844259a21358cd
Nghi Le <vinhnghi223@gmail.com>
2021-12-08 12:24:47 +0200

    added useData tests

---
+++ src/hooks/useData.test.tsx
@@ -0,0 +1,24 @@
+import { renderHook } from "@testing-library/react-hooks";
+import { useData } from "./useData";
+
+describe('useData', () => {
+    
+    describe('on fetch data success', () => {
+    
+        it('should populate Users, Posts and loadingStatus to Success', async () => {
+          const { result, waitForNextUpdate } = renderHook(() => useData());
+          await waitForNextUpdate();
+        })
+
+    })
+    
+      describe('on fetch data failure', () => {
+    
+        it('should populate Users, Posts and loadingStatus to Failure', async () => {
+          const { result, waitForNextUpdate } = renderHook(() => useData());
+          await waitForNextUpdate();
+        })
+        
+    })
+
+})
